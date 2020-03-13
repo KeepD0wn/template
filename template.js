140573c416dc4da5b4dcb6dd26d9965d7b5f1881
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2020-03-13 19:04:20 +0300

    Initial commit

---
+++ template.js
@@ -0,0 +1,40 @@
+$(function(){
+	
+	var header = $("#header");
+	var headerH = $("#header").innerHeight();
+	var introH = $("#intro").innerHeight();
+	var scrollOffset = $(window).scrollTop();
+	checkScroll(scrollOffset);
+
+	$("[data-scroll]").on("click",function(event){
+		event.preventDefault();
+		var blockId= $(this).data('scroll');
+		var blockOffset = $(blockId).offset().top;
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500) // время
+	});
+	
+	$(window).on("scroll",function(){
+		scrollOffset= $(this).scrollTop();
+		checkScroll(scrollOffset);
+	});
+
+	function checkScroll(scrollOffset){		
+		//console.log(scrollOffset);
+
+		if (scrollOffset > introH/2) {
+			header.addClass("fixed");
+		}
+		else{
+			header.removeClass("fixed");
+		}
+	}
+
+	$("#reg_btn").on("click",function(){
+		event.preventDefault();
+		$("#block").toggleClass("blackout");
+	});
+
+});
